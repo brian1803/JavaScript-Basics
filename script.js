651afe209d769fd9e889249eb85b057f3df0ddd5
brian1803 <134600@students.nccvt.k12.de.us>
2020-12-14 18:46:35 +0000

    practice1

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let name = prompt("Hi, Whats your name?");
+let job = prompt(" Hi, " +name "! " "What do you do?");
+
+let answer = prompt("Interesting! Do you love being a" +job "?");
+
+if ( answer= yes ) {
+    console.log( +name " is a " +job " and loves it!".toUpperCase);
+} else ( answer= no ) {
+    console.log( +name " is a " +job " and doesnt like it!".toUpperCase);
+}
